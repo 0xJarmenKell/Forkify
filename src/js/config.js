9db6157965a2e9e41940dd0a4b96281067a3f03a
youssef yousry <youssefyousry994@gmail.com>
2024-07-09 22:00:33 +0300

    init 1

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6b974021-97d3-4e43-8df2-8f5d64a02e6b';
+export const MODAL_CLOSE_SEC = 2.5;
